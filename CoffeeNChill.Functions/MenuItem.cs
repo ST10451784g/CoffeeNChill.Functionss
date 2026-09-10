4d2c33fea17baa047fd8ccd19b6d2d925b0055d7
Botho Rampedi <st10452447@myemeris.edu.za>
2026-09-10 20:24:28 +0200

    Add MenuItem entity class for Azure Table Storage

---
+++ CoffeeNChill.Functions/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace CoffeeNChill.Functions
+{
+    public class MenuItem : ITableEntity
+    {
+        // Business properties
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+
+        // Required by Azure Table Storage
+        public string PartitionKey { get; set; }  // e.g., "Hot Drinks"
+        public string RowKey { get; set; }        // e.g., "COF-001"
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
